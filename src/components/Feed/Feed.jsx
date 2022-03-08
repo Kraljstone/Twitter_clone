635cd39dfa315c6343f49910802682b80bacd6ba
Kralj Stone <arkham1001@gmail.com>
2022-03-08 13:08:50 +0100

    posts added

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classes from './feed.module.css';
+import Post from './Post/Post';
 import TweetBox from './TweetBox/TweetBox';
+import db from '../firebase';
+import { collection, onSnapshot } from 'firebase/firestore';
 
 const Feed = () => {
+  const [Posts, setPosts] = useState([{ name: 'Loading...', id: 'initial' }]);
+
+  useEffect(
+    () =>
+      onSnapshot(collection(db, 'Posts'), snapshot =>
+        setPosts(snapshot.docs.map(doc => ({ ...doc.data(), id: doc.id })))
+      ),
+    []
+  );
   return (
     <div className={classes.feed_container}>
       <div className={classes.feed_header}>
@@ -10,6 +22,19 @@ const Feed = () => {
       </div>
       <div className={classes.feed}>
         <TweetBox />
+      </div>
+      <div>
+        {Posts.map(post => (
+          <Post
+            key={post.id}
+            displayName={post.displayName}
+            username={post.username}
+            verified={post.verified}
+            text={post.text}
+            avatar={post.avatar}
+            image={post.image}
+          />
+        ))}
       </div>
     </div>
   );
---
+++ src/components/Feed/Post/Post.jsx
@@ -0,0 +1,47 @@
+import classes from './post.module.css';
+import { Avatar } from '@material-ui/core';
+import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import PublishIcon from '@material-ui/icons/Publish';
+
+const Post = ({ displayName, username, verified, text, image, avatar }) => {
+  return (
+    <div className={classes.post}>
+      <div className={classes.post__avatar}>
+        <Avatar src={avatar} />
+      </div>
+      <div className={classes.post__body}>
+        <div className={classes.post__header}>
+          <div className={classes.post__headerText}>
+            <h3>
+              {displayName}{' '}
+              <span className={classes.post__headerSpecial}>
+                {verified && (
+                  <VerifiedUserIcon className={classes.post__badge} />
+                )}{' '}
+                @{username}
+              </span>
+            </h3>
+          </div>
+          <div className={classes.post__headerDescription}>
+            <p>{text}</p>
+          </div>
+        </div>
+        <img src={image} alt="" />
+        <div className={classes.post__footer}>
+          <ChatBubbleOutlineIcon
+            className={classes.post_icons}
+            fontSize="small"
+          />
+          <RepeatIcon className={classes.post_icons} fontSize="small" />
+          <FavoriteBorderIcon className={classes.post_icons} fontSize="small" />
+          <PublishIcon className={classes.post_icons} fontSize="small" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
